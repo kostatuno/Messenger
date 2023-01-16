ac5b697693c43222462ba5c8814618b93722db1d
Zukicode <zukicode@gmail.com>
2023-01-16 16:57:38 +0200

    add first structure React-app

---
+++ messagenger-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="header"></div>
+			<div className="sidebar"></div>
+
+			<div className="content"></div>
+		</div>
+	);
+}
+
+export default App;
